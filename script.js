d490977ceb04272871c6f8297800812a8cab9699
nizacrz <cruz.yuniza@gmail.com>
2023-02-10 18:44:55 +0800

    finaal

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const showPassword = document.querySelector("#show-password");
-const passwordField = document.querySelector("#password");
+const togglePassword = document.querySelector("#hide1");
+const password = document.querySelector("#password1");
+const togglePassword1 = document.querySelector("#hide2");
+const password1 = document.querySelector("#password2");
 
-showPassword.addEventListener("click", function(){
-    this.classList.toggle("fa-eye-slash");
-    const type = passwordField.getAttribute("type")
-    === "password" ? "text" : "password";
-    passwordField.setAttribute("type", type);
-})
+        togglePassword.addEventListener("click", function () {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
+    
+    togglePassword1.addEventListener("click", function () {
+            // toggle the type attribute
+            const type = password1.getAttribute("type") === "password" ? "text" : "password";
+            password1.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
